b3f0c89732ecef7401aa5938d0996695150b45d9
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-06-21 15:25:05 -0500

    Controlador Invitacion 21 de junio

---
+++ controladores/invitacion.controlador.php
@@ -0,0 +1,61 @@
+<?php
+
+require "../modelos/registro.modelo.php";
+
+class invitationController{
+
+    public static function authValidate(){
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $requestData = json_decode(file_get_contents('php://input'), true);
+
+            $accion = $requestData['accion'];
+
+            if($accion === 'verificarCodigo'){
+
+                $tabla = "usuarios";
+                $item = "tokenGuest";
+                $clave = $requestData['clave'];
+                $nombre = $requestData['nombre'];
+                $apellido = $requestData['apellido'];
+                $tipo_documento = '0';
+                $identificacion = $requestData['identificacion'];
+                $dia_nacimiento = $requestData['dia_nacimiento'];
+                $mes_nacimiento = $requestData['mes_nacimiento'];
+                $anio_nacimiento = $requestData['anio_nacimiento'];
+                $genero = $requestData['genero'];
+                $direccion = $requestData['direccion'];
+                $ciudad = '0';
+                $telefono = $requestData['telefono'];
+                $celular = $requestData['celular'];
+                $correo_electronico = $requestData['correo_electronico'];
+                $contrasena = $requestData['contrasena'];
+                $confirmar_contrasena = $requestData['confirmar_contrasena'];
+                $accion = $requestData['accion'];
+
+                $response = ModeloRegistroFreeLancer::mdlBuscarCodigo($clave, $nombre, $apellido, $tipo_documento, $identificacion, $dia_nacimiento, $mes_nacimiento, $anio_nacimiento, $genero, $direccion, $ciudad, $telefono, $celular, $correo_electronico, $contrasena, $confirmar_contrasena, $accion, $tabla, $item) ;
+
+                if($response == true){
+
+                    $usuario = $identificacion;
+                    $response = ModeloRegistroFreeLancer::mdlRegistrarFreeLancer($nombre, $apellido, $direccion, $ciudad, $usuario, $confirmar_contrasena, $genero, $telefono, $correo_electronico, $dia_nacimiento, $mes_nacimiento, $anio_nacimiento, $tabla, $clave);
+
+                    if($response == true){
+                        $item = ModeloRegistroFreeLancer::mdlEliminarToken($usuario, $tabla);
+                    } else {
+                        echo json_encode(['error' => 'Error de registro']);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
+$validate = new invitationController();
+$validate->authValidate();  
+
+// var_dump($request);
+// die();
