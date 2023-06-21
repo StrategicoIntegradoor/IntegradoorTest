54a7c1755d05ea0e6d3d1fcffeab49b3794f7634
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-06-21 14:55:15 -0500

    vista Invitar

---
+++ vistas/modulos/invitar.php
@@ -0,0 +1,408 @@
+<head>
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta2/css/all.css" integrity="sha384-OA4SkQ1hW5kfQF3/OBdzK99bg7sQKT6+yXxq5Iu7QvGrrkrBsX3p5SRy9CrJ0+Gx" crossorigin="anonymous">
+
+</head>
+<style>
+  input[type="checkbox"] {
+    content: "";
+    width: 26px;
+    height: 26px;
+    border: 2px solid #ccc;
+    background: #ddd;
+  }
+
+  .contentnav {
+    display: table;
+    justify-content: space-around;
+    margin: auto;
+  }
+
+  .gray-header {
+        color: #82d600;
+    }
+
+  .divBoton {
+    display: flex;
+    justify-content: end;
+  }
+
+  .nav-tabs>.classli>.classa {
+    border: 1px solid lightgray;
+    color: black;
+  }
+
+
+  .nav-tabs>.classli.active>.classa,
+  .nav-tabs>.classli.active>.classa:focus,
+  .nav-tabs>.classli.active>.classa:hover {
+    color: #fff;
+    cursor: default;
+    background-color: #88d600;
+    border: 1px solid #ddd;
+    border-bottom-color: transparent;
+  }
+
+  /* Sweep To Bottom */
+  .botonSel {
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    position: relative;
+    -webkit-transition-property: color;
+    transition-property: color;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+  }
+
+  .botonSel:before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #88d600;
+    -webkit-transform: scaleY(0);
+    transform: scaleY(0);
+    -webkit-transform-origin: 50% 0;
+    transform-origin: 50% 0;
+    -webkit-transition-property: transform;
+    transition-property: transform;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-timing-function: ease-out;
+    transition-timing-function: ease-out;
+  }
+
+  .botonSel:hover,
+  .botonSel:focus,
+  .botonSel:active {
+    color: white;
+  }
+
+  .botonSel:hover:before,
+  .botonSel:focus:before,
+  .botonSel:active:before {
+    -webkit-transform: scaleY(1);
+    transform: scaleY(1);
+  }
+
+  .separador {
+    margin-left: 15px;
+  }
+
+  .smaller-input {
+  max-width: 200px;
+  margin: 0 auto;
+}
+
+.input-addon {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 325px;
+    z-index: 1;
+}
+/* .input-group {
+    display: flex;
+    align-items: center;
+} */
+
+.placeholder {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 6px;
+    color: #aaa;
+    pointer-events: none;
+    transition: all 0.2s;
+}
+
+.input-container {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+.input-container .form-control {
+        margin-left: 10px;
+    }
+
+.form {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.form input {
+  width: 90%;
+  height: 80px;
+  margin: 0.5rem;
+}
+
+.form button {
+  padding: 0.5em 1em;
+  border: none;
+  background: rgb(100, 200, 255);
+  cursor: pointer;
+}
+</style>
+
+<div class="content-wrapper">
+    <section class="content">
+        <h3>Invitación de registro freelance</h3>
+        <div class="box">
+        <h3 class="login-head text-info text-center gray-header" style="font-size: 24px; color: darkgreen; text-transform: uppercase; font-weight: bold; text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);">Enviar invitación de registro</h3>
+            <br>
+            <div class="form-container text-center">
+                <form style="width: 50%; margin: 0 auto;">
+                    <div class="row">
+                        <div class="col-md-12 offset-md-3">
+                            <div class="input-group" style="margin-bottom: 10px;">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-id-card-o" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control text-left" placeholder="Número de identificación" id="cc">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 offset-md-3">
+                            <div class="input-group" style="margin-bottom: 10px;">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control text-left" placeholder="Nombre Completo" id="name">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 offset-md-3">
+                            <div class="input-group" style="margin-bottom: 10px;">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control text-left" placeholder="Correo electrónico" id="mail">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="text-center" style="margin-bottom: 6px;">
+                            <div style="width: 30%; margin: 0 auto;">
+                                <button type="button" class="btn btn-primary btn-block" onclick="authCedula()">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <br>
+        </div>
+    </section>
+</div>
+
+
+<!-- <div class="content-wrapper">
+    <section class="content">
+        <h3>FORMULARIO</h3>
+        <div class="box">
+            <h3 class="login-head text-info text-center gray-header">Enviar invitación de registro</h3>
+            <br>
+            <div class="form-container text-center">
+                <form style="width: 50%; margin: 0 auto;">
+                    <div class="row">
+                        <div class="col-md-12 offset-md-3">
+                            <div class="input-group" style="margin-bottom: 10px;">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-id-card-o" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control text-right" placeholder="Número de identificación" id="cc">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 offset-md-3">
+                            <div class="input-group" style="margin-bottom: 10px;">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control text-right" placeholder="Nombre Completo" id="name">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 offset-md-3">
+                            <div class="input-group" style="margin-bottom: 10px;">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control text-right" placeholder="Correo electrónico" id="mail">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="text-center" style="margin-bottom: 20px;">
+                            <button type="button" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <br>
+        </div>
+    </section>
+</div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="content-wrapper">
+    <section class="content">
+        <h3>FORMULARIO</h3>
+        <div class="box">
+            <h3 class="login-head text-info text-center gray-header">Enviar invitación de registro</h3>
+            <br>
+            <div class="input-container" style="margin-bottom: 10px; max-width: 350px; margin: 0 auto;">
+                <input type="text" class="form-control" id="cc">
+                <span class="input-addon">
+                    <i class="fa fa-id-card-o" aria-hidden="true"></i>
+                </span>
+                <label for="cc" class="placeholder">Número de identificación</label>
+            </div>
+            <br>
+            <br>
+            <div class="input-container" style="margin-bottom: 10px; max-width: 350px; margin: 0 auto;">
+                <input type="text" class="form-control" id="name">
+                <span class="input-addon">
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                </span>
+                <label for="name" class="placeholder">Nombre Completo</label>
+            </div>
+            <br>
+            <br>
+            <div class="input-container" style="margin-bottom: 10px; max-width: 350px; margin: 0 auto;">
+                <input type="text" class="form-control" id="mail">
+                <span class="input-addon">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                </span>
+                <label for="mail" class="placeholder">Correo electrónico</label>
+            </div>
+            <br>
+            <br>
+            <div class="text-center" style="margin-bottom: 20px;">
+                <button type="button" class="btn btn-primary">Enviar</button>
+            </div>
+            <div><br></div>
+        </div>
+    </section>
+</div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+
+<div class="content">
+    <div class="content-wrapper">
+        <section class="content">
+            <section class="content-header">
+                <h1>Invitación freelance</h1>
+            </section>
+            <div class="box">
+            <h3 class="login-head text-info text-center gray-header">Enviar invitación de registro</h3>
+                <div style="padding: 15px; display: flex; flex-direction: column; align-items: center;">
+                    <div style="margin-bottom: 20px;">
+                        <div class="row">
+                            <div class="col-md-4 offset-md-4">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-id-card-o" aria-hidden="true"></i>
+                                    </span>
+                                    <input type="text" class="form-control" style="width: 700%;" placeholder="Número de identificación" id='cc'>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div style="margin-bottom: 20px;">
+                        <div class="row">
+                            <div class="col-md-4 offset-md-4">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                                    </span>
+                                    <input type="text" class="form-control" style="width: 700%;" placeholder="Nombre Completo" id='name'>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+
+                    <div style="margin-bottom: 20px;">
+                        <div class="row">
+                            <div class="col-md-4 offset-md-4">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                                    </span>
+                                    <input type="text" class="form-control" style="width: 700%;" placeholder="Correo electrónico" id='mail'>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+    
+                    <div style="margin-bottom: 10px;">
+                        <div class="row">
+                            <div class="col-md-4 offset-md-4">
+                                <button type="button" class="btn btn-primary" onclick="authCedula()">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div> -->
+
+
+
+
+
+
+<script src="vistas/js/invitar.js?v=<?php echo (rand()); ?>"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="vistas/js/usuarios.js?v=<?php echo (rand()); ?>"></script>
+<!-- <script src="vistas/js/invalidarPesadoDemo.js?v=<?php echo (rand()); ?>"></script> -->
