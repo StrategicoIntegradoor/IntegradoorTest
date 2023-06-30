d6e8e5a66f3d9b2ce4a2dfaf3f7729596e350705
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-06-30 16:58:12 -0500

    Ocultar asesor-editar-cotizacion

--- vistas/modulos/editar-cotizacion.php
+++ vistas/modulos/editar-cotizacion.php
@@ -606,6 +606,10 @@
           </div>
           <div id="contenCotizacionPDF">
             <div class="col-xs-12 col-sm-6 col-md-3">
+              <div class="col-xs-12 col-sm-6 col-md-3">
+                <label for="checkboxAsesor">¿Deseas agregar tus datos como asesor en la cotizacion?</label>
+                <input type="checkbox" id="checkboxAsesor">
+              </div>
               <button type="button" class="btn btn-danger btn-block" id="btnParrillaPDF">
                 <span class="fa fa-file-text"></span> Generar PDF de Cotización
               </button>
