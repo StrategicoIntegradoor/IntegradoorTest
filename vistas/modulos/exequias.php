888e472d5170faa42e49eccb8e27a3203b8418ca
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-07-13 07:23:25 -0500

    Actualización funcionalidad exequias

--- vistas/modulos/exequias.php
+++ vistas/modulos/exequias.php
@@ -436,7 +436,7 @@ select:invalid + .error-message {
                             </select>
                         </div>
                         <div class="col-xs-12 col-sm-6 col-md-3 form-group" style="margin-bottom: 0;" id="contenBtnConsultarExequial">
-                            <button class="btn btn-primary btn-block" id="btnExequial">Cotizar</button>
+                            <submit class="btn btn-primary btn-block" id="btnExequial">Cotizar</submit>
                         </div>
                     </div>
                 </form>
