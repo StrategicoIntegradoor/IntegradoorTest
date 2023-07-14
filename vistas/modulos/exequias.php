3e95f68c9231663a8a2ea926cca6e933db34197e
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-07-14 14:24:17 -0500

    Actualización funcionalidad exequias

--- vistas/modulos/exequias.php
+++ vistas/modulos/exequias.php
@@ -253,7 +253,7 @@ select:invalid + .error-message {
                                     </div>         
                                 </div>
                                 <div class="row-card-end">
-                                    <div style="text-align: center;">
+                                    <div style="text-align: center; margin-top: -40px;">
                                         <p style="margin-bottom: 0px; margin-top: 0; font-size: 13px; font-family: 'Arial Arabic', Arial, sans-serif;">AFILIADO TITULAR:</p>
                                         <P style="margin-bottom: 5%; margin-top: 0; font-size: 13px; font-family: 'Arial Arabic', Arial, sans-serif;">Titular hasta los 60 años</P>
                                         <p style="margin-bottom: 0px; font-size: 13px; font-family: 'Arial Arabic', Arial, sans-serif;">Máx. 8 afiliados adicionales por titular</p>
