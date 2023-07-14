9c7fc7ab52cbeab5c21a7275f8c96bd15eaddb16
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-07-14 12:58:59 -0500

    Actualización funcionalidad ocultar_asesor

--- vistas/modulos/editar-cotizacion.php
+++ vistas/modulos/editar-cotizacion.php
@@ -607,13 +607,13 @@
           <div id="contenCotizacionPDF">
             <div class="col-xs-12 col-sm-6 col-md-10">
               <!-- <div class="row"> -->
-                <div class="col-xs-3">
+                <div class="col-xs-4">
                   <label for="checkboxAsesor">¿Deseas agregar tus datos como asesor en la cotizacion?</label>
                 </div>
-                <div class="col-xs-1">
+                <div class="col-xs-2">
                   <input type="checkbox" id="checkboxAsesorEditar">
                 </div>
-                <div class="col-xs-4">
+                <div class="col-xs-6">
                   <button type="button" class="btn btn-danger btn-block" id="btnParrillaPDF">
                     <span class="fa fa-file-text"></span> Generar PDF de Cotización
                   </button>
