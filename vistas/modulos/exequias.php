f1316d4d17a7db3a32ed768b6a119f19b68114ca
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-07-14 14:34:06 -0500

    Actualización funcionalidad exequias

--- vistas/modulos/exequias.php
+++ vistas/modulos/exequias.php
@@ -252,7 +252,7 @@ select:invalid + .error-message {
                                         <p style="margin-bottom: 0px; margin-top: 0;">Pago anual</p>
                                     </div>         
                                 </div>
-                                <div class="row-card-end" style="text-align: center; margin-top: -10px;">
+                                <div class="row-card-end" style="text-align: center;">
                                     <!-- <div style="text-align: center;"> -->
                                         <p style="margin-bottom: 0px; margin-top: -40px; font-size: 13px; font-family: 'Arial Arabic', Arial, sans-serif;">AFILIADO TITULAR:</p>
                                         <P style="margin-bottom: 5%; margin-top: 0; font-size: 13px; font-family: 'Arial Arabic', Arial, sans-serif;">Titular hasta los 60 años</P>
@@ -260,6 +260,7 @@ select:invalid + .error-message {
                                         <!-- <p style="margin-bottom: 0px; margin-top: 0; font-size: 13px; font-family: 'Arial Arabic', Arial, sans-serif; color: transparent;">.</p> -->
                                     <!-- </div> -->
                                 </div>
+                                <div></div>
                             </div>
                         </div>
                         <!-- //PLAN NUESTRA FAMILIA -->
