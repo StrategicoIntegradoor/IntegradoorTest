a2e02e1c48f34de8b4710879d08b98a3f6227e93
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-07-14 11:36:28 -0500

    Actualización funcionalidad exequias

--- vistas/modulos/exequias.php
+++ vistas/modulos/exequias.php
@@ -242,7 +242,7 @@ select:invalid + .error-message {
                             <div class="rounded-container" style="height: 100%;">
                                 <div class="row-card" style="display: flex; align-items: start;">
                                     <div class="col-md-4 col-sm-4" style="display: flex; align-items: center; justify-content: center;">
-                                        <div class="circle" style="aspect-ratio: 1/1; max-width: 100px;">
+                                        <div class="circle" style="aspect-ratio: 1/1;">
                                             <img src="vistas/img/plantilla/plan_personal.jpg" style="max-width: 100%; max-height: 100%; object-fit: contain;">
                                         </div>
                                     </div>
@@ -270,7 +270,7 @@ select:invalid + .error-message {
                             <div class="rounded-container" style="height: 100%;">
                                 <div class="row-card" style="display: flex; align-items: start;">
                                     <div class="col-md-4 col-sm-4" style="display: flex; align-items: center; justify-content: center;">
-                                        <div class="circle" style="aspect-ratio: 1/1; max-width: 100px;" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+                                        <div class="circle" style="aspect-ratio: 1/1;" style="max-width: 100%; max-height: 100%; object-fit: contain;">
                                             <img src="vistas/img/plantilla/plan_familiar.jpg">
                                         </div>
                                     </div>
