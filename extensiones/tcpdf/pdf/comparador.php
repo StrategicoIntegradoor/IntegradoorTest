7e28ba5ef28ef78a9ddad1b02b480e28cbacb040
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-08-17 08:22:34 -0500

    Pruebas cotizaciones Liberty

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -1631,7 +1631,7 @@ $html4 .= '</table>';
 //CONSULTA ResponsabilidadCivilGeneralFamiliar
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-$html4 .= '<table style="width: 100%;" class="second2" cellpadding="2"  border="0">';
+$html4 .= '<table style="width: 100%; vertical-align: middle;" class="second2" cellpadding="2"  border="0">';
 
 $html4 .= '<tr>';
 $html4 .= '<td class="fondo puntos" style="width:10%; text-align: center; font-family: dejavusanscondensedb; vertical-align: middle; display: table-cell;"><font size="8">RC familiar</font></td>';
