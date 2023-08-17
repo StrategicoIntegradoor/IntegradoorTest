71c58c5ae38dd82b6943b8c1a70cc8588c07ef88
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-08-17 12:33:48 -0500

    Pruebas cotizaciones HDI

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -1575,7 +1575,7 @@ function cotizarOfertas() {
                   return res.json();
                 })
                 .then((ofertas) => {
-                  console.log(ofertas)
+                  console.log(ofertas['CotizarXGLMResult'])
                   if (typeof ofertas[0].Resultado !== 'undefined') {
                     agregarAseguradoraFallida('HDI')
                     ofertas[0].Mensajes.forEach(mensaje => {
