c46cd513b1fe44dc7fff052c89a265357de57613
jhonalbear <112020348+jhonalbear@users.noreply.github.com>
2023-09-02 10:14:11 -0500

    prueba enter (#5)
    Co-authored-by: Jhon1752321 <63821027+Jhon1752321@users.noreply.github.com>

--- src/insertarOferta.php
+++ src/insertarOferta.php
@@ -4,6 +4,7 @@
 require_once("../config/db.php"); //Contiene las variables de configuracion para conectar a la base de datos
 require_once("../config/conexion.php"); //Contiene funcion que conecta a la base de datos
 
+
 $placa = $_POST['placa'];
 $idCotizacion = $_POST['idCotizOferta'];
 $numIdentificacion = $_POST['numIdentificacion'];
