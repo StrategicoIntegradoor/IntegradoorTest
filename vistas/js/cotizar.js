70b0122c052b6fc63ed4c2a77f703ddf7765d131
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-11 12:17:01 -0500

    Mejoras cotizar

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -1423,7 +1423,7 @@ function cotizarOfertas() {
               document.querySelector('.fallidas').innerHTML += `<p><i class="fa fa-times" aria-hidden="true" style="color: red; margin-right: 10px;"></i><b>${aseguradora}:</b> ${mensaje}</p>`
             }
 
-            let contadorOfertas = 0;
+            let contadorTarjetas = 0;
 
             /* Solidaria */
             cont.push(
