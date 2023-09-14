b0e683ffcb635dceb4c7f04bf091f64b22f7b698
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-14 12:22:45 -0500

    Mejoras Módulo Usuarios

--- vistas/js/usuarios.js
+++ vistas/js/usuarios.js
@@ -221,12 +221,13 @@ $("#ingciudadCirculacion").select2({
 	});
 
 // Carga los Intermediarios disponibles para agregar
-$("#idIntermediario").select2({
-	theme: "bootstrap dpto1",
-	language: "es",
-	width: "100%",
-	placeholder: "Intermediario",
-	});
+// $("#idIntermediario").select2({
+// 	theme: "bootstrap dpto1",
+// 	language: "es",
+// 	width: "100%",
+// 	placeholder: "Intermediario",
+// 	});
+
 // FUNCION PARA CARGAR LA CIUDAD DE CIRCULACIÓN
 function consultarCiudad() {
 	var codigoDpto = document.getElementById("DptoCirculacion").value;
