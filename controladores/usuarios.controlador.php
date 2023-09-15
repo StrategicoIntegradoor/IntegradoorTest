537cc3a4e5436fe8089be20709058768f19f259c
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-15 10:33:44 -0500

    Bloqueo de usuarios

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -81,6 +81,7 @@ class ControladorUsuarios{
 							title: "Usuario Inhabilitado",
 							html: "Hola 😔, lamentamos comunicarte que tu usuario como aliado de Grupo Asistencia ha sido inhabilitado. Si deseas reactivarlo, debes realizar compromiso de producción y comunicarte con el área de vinculaciones de Grupo Asistencia al 📲 +573185127910 o vía 📧 analistadeseguros@grupoasistencia.com. Si no estás interesado en vender seguros por medio de Grupo Asistencia como aliado pero te interesa tener tu propia versión personalizada del software, comunícate con nosotros, Strategico Technologies, desarrolladores de esta plataforma, para conocer acerca de los planes de pago, que inician desde los $1.950 pesos por placa cotizada. Contacto: Strategico Technologies +573187664954 proyectos@strategico.tech",
 							icon: "error",
+							width: "80%", // Personaliza el ancho aquí (puedes usar porcentaje o píxeles)
 						}).then(function () {
 							window.location.href = "login.php"; // Redirigir después de cerrar SweetAlert
 						});
