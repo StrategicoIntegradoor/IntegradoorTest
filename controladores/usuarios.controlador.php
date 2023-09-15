f963ff2fcca11a52688a73f5db240bfaaf2f50f8
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-15 14:55:32 -0500

    Bloqueo de usuarios

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -111,7 +111,7 @@ class ControladorUsuarios{
 											proyectos@strategico.tech
 										</div>
 								`,
-									width: "44%", // Personaliza el ancho aquí (puedes usar porcentaje o píxeles)
+									width: "80%", // Personaliza el ancho aquí (puedes usar porcentaje o píxeles)
 									customClass: {
 										container: "swal-container",
 										title: "swal-title",
