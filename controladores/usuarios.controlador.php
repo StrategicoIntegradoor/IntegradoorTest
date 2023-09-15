48b984486d9182d086e177d112af7c9fad359673
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-15 10:15:41 -0500

    Bloqueo de usuarios

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -88,12 +88,15 @@ class ControladorUsuarios{
 						// 	proyectos@strategico.tech</div>';
 
 						echo 	'<script>
-									swal({
-										title: "Usuario Inhabilitado",
-										text: "Hola 😔, lamentamos comunicarte que tu usuario como aliado de Grupo Asistencia ha sido inhabilitado. Si deseas reactivarlo, debes realizar compromiso de producción y comunicarte con el área de vinculaciones de Grupo Asistencia al 📲 +573185127910 o vía 📧 analistadeseguros@grupoasistencia.com. Si no estás interesado en vender seguros por medio de Grupo Asistencia como aliado pero te interesa tener tu propia versión personalizada del software, comunícate con nosotros, Strategico Technologies, desarrolladores de esta plataforma, para conocer acerca de los planes de pago, que inician desde los $1.950 pesos por placa cotizada. Contacto: Strategico Technologies +573187664954 proyectos@strategico.tech",
-										icon: "error",
-									});
-								</script>';
+						function enviarFormulario() {
+							swal({
+								title: "Usuario Inhabilitado",
+								text: "Hola 😔, lamentamos comunicarte que tu usuario como aliado de Grupo Asistencia ha sido inhabilitado. Si deseas reactivarlo, debes realizar compromiso de producción y comunicarte con el área de vinculaciones de Grupo Asistencia al 📲 +573185127910 o vía 📧 analistadeseguros@grupoasistencia.com. Si no estás interesado en vender seguros por medio de Grupo Asistencia como aliado pero te interesa tener tu propia versión personalizada del software, comunícate con nosotros, Strategico Technologies, desarrolladores de esta plataforma, para conocer acerca de los planes de pago, que inician desde los $1.950 pesos por placa cotizada. Contacto: Strategico Technologies +573187664954 proyectos@strategico.tech",
+								icon: "error",
+							});
+							return false; // Evita que el formulario se envíe
+						}
+					 </script>';
 
 					}
 				}else{
