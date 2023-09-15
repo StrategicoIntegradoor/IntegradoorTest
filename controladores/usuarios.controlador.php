af78567a039d164572ff3d9fb85eb40632ed8d9b
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-15 15:31:40 -0500

    Bloqueo de usuarios

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -130,7 +130,7 @@ class ControladorUsuarios{
 							<style>
 								.swal-confirm-button {
 									margin-top: 3px; /* Ajusta el margen superior para reducir el espacio entre el botón y el texto */
-									font-size: 21px; /* Aumenta el tamaño del botón */
+									font-size: 28px; /* Aumenta el tamaño del botón */
 									padding: 11px 30px; /* Ajusta el padding para hacer que el botón sea más grande */
 								}
 							</style>';
