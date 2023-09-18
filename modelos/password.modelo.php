ddcbd43c9cd2c3b0d9683bf32c439f3e6b815336
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-18 11:50:02 -0500

    Imagen Cambio de contraseña e invitacion

--- modelos/password.modelo.php
+++ modelos/password.modelo.php
@@ -147,11 +147,7 @@ class ModeloPassword{
             </head>
             
             <body>
-                <!--[if gte mso 9]>
-                    <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
-                        <v:fill type="tile" src="https://www.integradoor.com/app/vistas/img/iregistro1.jpeg" color="#7bceeb"/>
-                    </v:background>
-                <![endif]-->
+                
                 <div class="container" background="https://www.integradoor.com/app/vistas/img/iregistro1.jpeg"> 
                     <table style="background-size:100% 100%;height:800px;width:600px" border="0" cellspacing="0" cellpadding="20" background="https://www.integradoor.com/app/vistas/img/iregistro1.jpeg">
                         <tr>
