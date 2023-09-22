00c0f4041d75e3d5034ae0d280e25bad224adea1
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-22 16:14:02 -0500

    Mejoras modulo de invitacion

--- vistas/modulos/invitacion.php
+++ vistas/modulos/invitacion.php
@@ -691,14 +691,17 @@
 
                 <form method="post">
                     
-                    <div class="row">
-                        <hr>
-                        <label for="" class="login-box-msg">Los campos con <strong class="text-danger">*</strong> son obligatorios</label><br><br>
-                        <div class="col-xs-12 col-sm-12 col-md-12 form-group ">
-                            <label for="">Clave de registro <strong class="text-danger">*</strong> </label>
-                            <input class="form-control" type="text" name="clave_registro" id="clave_registro" placeholder="Clave de registro" autofocus>
+                    <div class="col-lg-12 form-resumAseg">                                                                                    
+                        <div class="row">
+                            <hr>
+                            <label for="" class="login-box-msg">Los campos con <strong class="text-danger">*</strong> son obligatorios</label><br><br>
+                            <div class="col-xs-12 col-sm-12 col-md-12 form-group ">
+                                <label for="">Clave de registro <strong class="text-danger">*</strong> </label>
+                                <input class="form-control" type="text" name="clave_registro" id="clave_registro" placeholder="Clave de registro" autofocus>
+                            </div>
                         </div>
                     </div>
+
 
                     <div class="row">
                         <div class="form-group col-md-6 col-sm-12 col-xs-12">
