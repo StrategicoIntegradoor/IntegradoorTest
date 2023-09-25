ce1228c5b22da128cc56f25c76758c5c2dc30ab2
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-09-25 11:46:02 -0500

    Mantenimiento modulo de usuarios

--- vistas/js/usuarios.js
+++ vistas/js/usuarios.js
@@ -10,21 +10,7 @@ cargarRoll();
 
   
 
-// // Función para validar el campo de teléfono
-// function validarTelefono() {
-// 	const telefono = document.getElementById("AgregMovil").value;
-  
-// 	if (telefono.length < 10) {
-// 	  // El teléfono tiene menos de 10 caracteres, mostrar mensaje de error
-// 	  document.getElementById("mensajeError").innerHTML = "El número de teléfono debe tener al menos 10 caracteres.";
-// 	} else {
-// 	  // El teléfono es válido, borrar mensaje de error
-// 	  document.getElementById("mensajeError").innerHTML = "";
-// 	}
-//   }
-  
-//   // Agregar un event listener para llamar a la función de validación cuando el usuario escriba en el campo
-//   document.getElementById("AgregMovil").addEventListener("input", validarTelefono());
+
   
 
 	function cargarIntermediario(){
@@ -706,3 +692,18 @@ $("#editarDocIdUser").keyup(function(){
 
 });
 
+// Función para validar el campo de teléfono
+function validarTelefono() {
+	const telefono = document.getElementById("AgregMovil").value;
+  
+	if (telefono.length < 10) {
+	  // El teléfono tiene menos de 10 caracteres, mostrar mensaje de error
+	  document.getElementById("mensajeError").innerHTML = "El número de teléfono debe tener al menos 10 caracteres.";
+	} else {
+	  // El teléfono es válido, borrar mensaje de error
+	  document.getElementById("mensajeError").innerHTML = "";
+	}
+  }
+  
+  // Agregar un event listener para llamar a la función de validación cuando el usuario escriba en el campo
+  document.getElementById("AgregMovil").addEventListener("input", validarTelefono());
