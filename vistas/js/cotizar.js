5ac973a1d16aff9cfba35e810c5832730b10eeb7
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-02 16:22:18 -0500

    Revision Zurich

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -1725,31 +1725,31 @@ function cotizarOfertas() {
               body.Email2 = Math.round(Math.random() * 999999) + body.Email
               console.log(body.Email2)
               requestOptions.body = JSON.stringify(body)
-              // cont.push(
-              //   fetch('https://grupoasistencia.com/motor_webservice_tst/Zurich', requestOptions)
-              //     .then(res => {
-              //       if (!res.ok) throw Error(res.statusText)
-              //       return res.json()
-              //     })
-              //     .then(ofertas => {
-              //       if (typeof ofertas.Resultado !== 'undefined') {
-              //         agregarAseguradoraFallida('Zurich')
-              //         if (zurichErrors) {
-              //           ofertas.Mensajes.forEach(mensaje => {
-              //             mostrarAlertarCotizacionFallida(`Zurich ${plan}`, mensaje)
-              //           })
-              //         }
-              //         zurichErrors = false
-              //       } else {
-              //         validarOfertas(ofertas)
-              //         if (zurichSuccess) {
-              //           mostrarAlertaCotizacionExitosa('Zurich')
-              //           zurichSuccess = false
-              //         }
-              //       }
-              //     })
-              //     .catch(err => console.error(err))
-              // )
+              cont.push(
+                fetch('https://grupoasistencia.com/motor_webservice_tst/Zurich', requestOptions)
+                  .then(res => {
+                    if (!res.ok) throw Error(res.statusText)
+                    return res.json()
+                  })
+                  .then(ofertas => {
+                    if (typeof ofertas.Resultado !== 'undefined') {
+                      agregarAseguradoraFallida('Zurich')
+                      if (zurichErrors) {
+                        ofertas.Mensajes.forEach(mensaje => {
+                          mostrarAlertarCotizacionFallida(`Zurich ${plan}`, mensaje)
+                        })
+                      }
+                      zurichErrors = false
+                    } else {
+                      validarOfertas(ofertas)
+                      if (zurichSuccess) {
+                        mostrarAlertaCotizacionExitosa('Zurich')
+                        zurichSuccess = false
+                      }
+                    }
+                  })
+                  .catch(err => console.error(err))
+              )
             })
 
             let successEstado = true
