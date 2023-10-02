48b33018c434cede1d47ef113d73bc33e6ce0772
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-02 09:35:29 -0500

    PDF nuevas parametrizaciones

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -1959,7 +1959,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
     $rowRespuestaAsistencia22 = mysqli_fetch_assoc($respuestaqueryAsistencia22);
 	
     if ($cont26 % 2 == 0) {
-        if ($PT == "Deducible: 10% min 1.2 SMMLV" || "Deducible: 10% min 1 SMMLV") {
+        if ($PT == "Deducible: 10% min 1.2 SMMLV" || $PT == "Deducible: 10% min 1 SMMLV") {
 			var_dump("Primer condicion");
 			die();
 			$html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><div style="font-size:14pt">&nbsp;</div><font size="7"style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
@@ -1975,7 +1975,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
             }
         }
     } else {
-        if ($PT == "Deducible: 10% min 1 SMMLV" || "Deducible: 10% min 1 SMMLV") {
+        if ($PT == "Deducible: 10% min 1 SMMLV" || $PT == "Deducible: 10% min 1 SMMLV") {
 			var_dump($PT);
 			die();
 			$html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><div style="font-size:14pt">&nbsp;</div><font size="7"style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
