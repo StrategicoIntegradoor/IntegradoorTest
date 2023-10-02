f60557a14485a08d894b84ad34cc5dbc638a768e
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-02 08:58:05 -0500

    PDF nuevas parametrizaciones

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -1959,7 +1959,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
     $rowRespuestaAsistencia22 = mysqli_fetch_assoc($respuestaqueryAsistencia22);
 
     if ($cont26 % 2 == 0) {
-        if ($PT == "Deducible: 10% min 1.2 SMMLV") {
+        if ($PT == "Deducible: 10% min 1.2 SMMLV" || $PT == "Deducible: 10% min 1 SMMLV") {
 			$html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><div style="font-size:14pt">&nbsp;</div><font size="7" style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
         } else {
             if ($rowRespuestaAsistencia22['GastosMedicos'] == "Si ampara") {
@@ -1970,9 +1970,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
             }
         }
     } else {
-        if ($PT == "Deducible: 10% min 1 SMMLV") {
 			$html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><div style="font-size:14pt">&nbsp;</div><font size="7" style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
-        } else {
             if ($rowRespuestaAsistencia22['GastosMedicos'] == "Si ampara") {
                 $html4 .= '<td class="puntos fondo2" style="width:' . $valorTabla . '%;text-align: center;"><div style="font-size:12pt">&nbsp;</div><img style="width:16px;" src="../../../vistas/img/logos/cheque.png" alt=""></td>';
             } else {
@@ -1983,7 +1981,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
     }
 
     $cont26 += 1;
-}
+
 
 $html4 .= '</tr>';
 $html4 .= '</table>';
