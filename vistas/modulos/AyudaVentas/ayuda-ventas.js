9c638bfc2166fc6c342766dd795bb4dbf8a93f55
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-09 10:48:01 -0500

    Modulo ayudaventas

--- vistas/modulos/AyudaVentas/ayuda-ventas.js
+++ vistas/modulos/AyudaVentas/ayuda-ventas.js
@@ -166,10 +166,10 @@ const obtenerAyudaVentas = async () => {
             }
             if (ayudaVenta.path_sarlaft || ayudaVenta.path_sarlaft2) {
                 let sarlaftButtons = '<td style="line-height: 200px;">'
-                sarlaftButtons += ayudaVenta.path_sarlaft ? `<button class="btn btn-alert" style="background: red; color: #fff; font-weight: 500;" onclick="onclick="validarPermisoPdfPersonaNatural('https://allianzfcc.co/#/login')">PDF PN</button>` : ''
+                sarlaftButtons += ayudaVenta.path_sarlaft ? `<button class="btn btn-alert" style="background: red; color: #fff; font-weight: 500;" onclick="onclick="validarPermisoPdfPersonaNatural('./vistas/modulos/AyudaVentas/pdf/sarlaft/${ayudaVenta.path_sarlaft}')">PDF PN</button>` : ''
                 partTemplate += sarlaftButtons + '</td>'
                 let sarlaftButtons2 = '<td style="line-height: 200px;">'
-                sarlaftButtons2 += ayudaVenta.path_sarlaft2 ? `<button class="btn btn-alert" style="background: red; color: #fff; font-weight: 500;" onclick="validarPermisoPdfPersonaJuridica('https://allianzfcc.co/#/login')">PDF PJ</button>` : ''
+                sarlaftButtons2 += ayudaVenta.path_sarlaft2 ? `<button class="btn btn-alert" style="background: red; color: #fff; font-weight: 500;" onclick="validarPermisoPdfPersonaJuridica('./vistas/modulos/AyudaVentas/pdf/sarlaft2/${ayudaVenta.path_sarlaft2}')">PDF PJ</button>` : ''
                 partTemplate += sarlaftButtons2 + '</td>'
             } else {
                 partTemplate += '<td></td>'
