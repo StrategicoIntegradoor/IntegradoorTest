a3d0705fa4deccdc5755b8446115c296ac1db947
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-17 14:47:37 -0500

    Modulo pesados

--- vistas/js/pesados.js
+++ vistas/js/pesados.js
@@ -1031,7 +1031,7 @@ function registrarOferta(
     pdf,
   ) {
     return new Promise((resolve, reject) => {
-      var idCotizOferta = numCotizOferta
+      var idCotizOferta = idCotizacion
       var numDocumentoID = document.getElementById("numDocumentoID").value
       var placa = document.getElementById("placaVeh").value
       if (manual == null) { 
