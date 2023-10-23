0c93da7c435b3f58b47f6c07fb5c38f10f1a5926
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-23 12:00:25 -0500

    Modulo pesados Mundial

--- extensiones/tcpdf/pdf/comparadorPesados.php
+++ extensiones/tcpdf/pdf/comparadorPesados.php
@@ -988,10 +988,10 @@ while ($rowRespuesta8 = mysqli_fetch_assoc($respuestaquery8)) {
 									AND `rce` LIKE '$valorRC' AND `ppd` LIKE '$perdidaParcial'";
 	$respuestaqueryAsistencia1 =  $conexion->query($queryConsultaAsistencia1);
 	$rowRespuestaAsistencia1 = mysqli_fetch_assoc($respuestaqueryAsistencia1);
-	var_dump($nombreProducto);
-	var_dump($valorRC);
-	var_dump($perdidaParcial);
-	die();
+	// var_dump($nombreProducto);
+	// var_dump($valorRC);
+	// var_dump($perdidaParcial);
+	// die();
 	if ($cont5 % 2 == 0) {
 		$html3 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><font size="7"style="text-align: center;  font-family:dejavusanscondensed;">' . $rowRespuestaAsistencia1['deducible'] . '</font></center></td>';
 	} else {
