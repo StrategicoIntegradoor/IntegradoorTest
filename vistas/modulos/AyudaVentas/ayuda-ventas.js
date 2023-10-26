302f6c636daa2b64959052598127f90462aee40f
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-26 14:32:06 -0500

    Cambios Ayudaventas

--- vistas/modulos/AyudaVentas/ayuda-ventas.js
+++ vistas/modulos/AyudaVentas/ayuda-ventas.js
@@ -245,7 +245,7 @@ const obtenerAyudaVentas = async () => {
             let partTemplate = `
                 <tr >
                     <td style="max-width: 5%; white-space: nowrap; text-align: center;"><img src="./vistas/modulos/AyudaVentas/src/logos/${ayudaVenta.aseguradora}.png" style="margin-top: 60px;" class="img-responsive" width="80"></td>
-                    <td style="line-height: 200px;" style="max-width: 5%; white-space: nowrap; text-align: center;">${ayudaVenta.linea_de_atencion}</td>`
+                    <td style="line-height: 200px;">${ayudaVenta.linea_de_atencion}</td>`
             if (ayudaVenta.link_clausulado) {
                 partTemplate += `<td style="line-height: 200px;" style="max-width: 5%; white-space: normal; text-align: center;" ><button class="btn btn-alert" style="border-color: #88d600; width: 205px; color: #88d600; font-weight: 500;" onclick="validarPermisoClausulado('${ayudaVenta.link_clausulado}')">${ayudaVenta.link_clausulado.substring(0,27)}</button></td>`
             } else {
