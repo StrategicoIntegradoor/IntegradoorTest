858e2e7ec49b1fb3724db90e9cdff0b7918172c5
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-26 16:25:59 -0500

    Cambios Ayudaventas

--- vistas/modulos/AyudaVentas/ayuda-ventas.js
+++ vistas/modulos/AyudaVentas/ayuda-ventas.js
@@ -245,17 +245,17 @@ const obtenerAyudaVentas = async () => {
             let partTemplate = `
                 <tr >
                     <td><img src="./vistas/modulos/AyudaVentas/src/logos/${ayudaVenta.aseguradora}.png" style="margin-top: 60px;" class="img-responsive" width="80"></td>
-                    <td style="line-height: 200px;">${ayudaVenta.linea_de_atencion}</td>`
+                    <td>${ayudaVenta.linea_de_atencion}</td>`
             if (ayudaVenta.link_clausulado) {
-                partTemplate += `<td style="line-height: 200px;"><button class="btn btn-alert" style="border-color: #88d600; width: 205px; color: #88d600; font-weight: 500;" onclick="validarPermisoClausulado('${ayudaVenta.link_clausulado}')">${ayudaVenta.link_clausulado.substring(0,27)}</button></td>`
+                partTemplate += `<td><button class="btn btn-alert" style="border-color: #88d600; width: 205px; color: #88d600; font-weight: 500;" onclick="validarPermisoClausulado('${ayudaVenta.link_clausulado}')">${ayudaVenta.link_clausulado.substring(0,27)}</button></td>`
             } else {
                 partTemplate += '<td></td>'
             }
             if (ayudaVenta.path_sarlaft || ayudaVenta.path_sarlaft2) {
-                let sarlaftButtons = '<td style="line-height: 200px;">'
+                let sarlaftButtons = '<td>'
                 sarlaftButtons += ayudaVenta.path_sarlaft ? `<button class="btn btn-alert" style="background: red; color: #fff; font-weight: 500;" onclick="onclick="validarPermisoPdfPersonaNatural('./vistas/modulos/AyudaVentas/pdf/sarlaft/${ayudaVenta.path_sarlaft}')">PDF PN</button>` : ''
                 partTemplate += sarlaftButtons + '</td>'
-                let sarlaftButtons2 = '<td style="line-height: 200px;">'
+                let sarlaftButtons2 = '<td>'
                 sarlaftButtons2 += ayudaVenta.path_sarlaft2 ? `<button class="btn btn-alert" style="background: red; color: #fff; font-weight: 500;" onclick="validarPermisoPdfPersonaJuridica('./vistas/modulos/AyudaVentas/pdf/sarlaft2/${ayudaVenta.path_sarlaft2}')">PDF PJ</button>` : ''
                 partTemplate += sarlaftButtons2 + '</td>'
             } else {
