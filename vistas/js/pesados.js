43ed2d75bf2cc51002177e660da1577fb99dcc09
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-31 09:01:12 -0500

    Union de versiones cotizar.js

--- vistas/js/pesados.js
+++ vistas/js/pesados.js
@@ -424,164 +424,164 @@ function masAseg() {
   var contErrMetEstado = 0;
   var contErrProtocolo = 0;
 
-  function consulPlaca() {
-    var numplaca = document.getElementById("placaVeh").value;
-    var valnumplaca = numplaca.toUpperCase(); // Convierte la Placa en Mayusculas
-    var tipoDocumentoID = document.getElementById("tipoDocumentoID").value;
-    var numDocumentoID = document.getElementById("numDocumentoID").value;
-    var dianacimiento = document.getElementById("dianacimiento").value;
-    var mesnacimiento = document.getElementById("mesnacimiento").value;
-    var anionacimiento = document.getElementById("anionacimiento").value;
-    var nombresAseg = document.getElementById("txtNombres").value;
-    var apellidosAseg = document.getElementById("txtApellidos").value;
-    var generoAseg = document.getElementById("genero").value;
-    var estadoCivil = document.getElementById("estadoCivil").value;
-    var intermediario = document.getElementById("intermediario").value;
+  // function consulPlaca() {
+  //   var numplaca = document.getElementById("placaVeh").value;
+  //   var valnumplaca = numplaca.toUpperCase(); // Convierte la Placa en Mayusculas
+  //   var tipoDocumentoID = document.getElementById("tipoDocumentoID").value;
+  //   var numDocumentoID = document.getElementById("numDocumentoID").value;
+  //   var dianacimiento = document.getElementById("dianacimiento").value;
+  //   var mesnacimiento = document.getElementById("mesnacimiento").value;
+  //   var anionacimiento = document.getElementById("anionacimiento").value;
+  //   var nombresAseg = document.getElementById("txtNombres").value;
+  //   var apellidosAseg = document.getElementById("txtApellidos").value;
+  //   var generoAseg = document.getElementById("genero").value;
+  //   var estadoCivil = document.getElementById("estadoCivil").value;
+  //   var intermediario = document.getElementById("intermediario").value;
   
-    if (
-      numplaca != "" &&
-      tipoDocumentoID != "" &&
-      numDocumentoID != "" &&
-      dianacimiento != "" &&
-      mesnacimiento != "" &&
-      anionacimiento != "" &&
-      nombresAseg != "" &&
-      apellidosAseg != "" &&
-      generoAseg != "" &&
-      estadoCivil != ""
-    ) {
-      // Oculta los campos de consultar Vehiculo paso a paso desde la Guia Fasecolda
-      document.getElementById("formularioVehiculo").style.display = "none";
-      $("#loaderPlaca").html(
-        '<img src="vistas/img/plantilla/loader-loading.gif" width="34" height="34"><strong> Consultando Placa...</strong>'
-      );
+  //   if (
+  //     numplaca != "" &&
+  //     tipoDocumentoID != "" &&
+  //     numDocumentoID != "" &&
+  //     dianacimiento != "" &&
+  //     mesnacimiento != "" &&
+  //     anionacimiento != "" &&
+  //     nombresAseg != "" &&
+  //     apellidosAseg != "" &&
+  //     generoAseg != "" &&
+  //     estadoCivil != ""
+  //   ) {
+  //     // Oculta los campos de consultar Vehiculo paso a paso desde la Guia Fasecolda
+  //     document.getElementById("formularioVehiculo").style.display = "none";
+  //     $("#loaderPlaca").html(
+  //       '<img src="vistas/img/plantilla/loader-loading.gif" width="34" height="34"><strong> Consultando Placa...</strong>'
+  //     );
   
-      //INICIO DE CABECERA PARA INGRESAR INFORMACION DEL METODO
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
+  //     //INICIO DE CABECERA PARA INGRESAR INFORMACION DEL METODO
+  //     var myHeaders = new Headers();
+  //     myHeaders.append("Content-Type", "application/json");
   
-      var raw = JSON.stringify({ Placa: valnumplaca, intermediario: intermediario });
+  //     var raw = JSON.stringify({ Placa: valnumplaca, intermediario: intermediario });
   
-      var requestOptions = {
-        mode: "cors",
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
+  //     var requestOptions = {
+  //       mode: "cors",
+  //       method: "POST",
+  //       headers: myHeaders,
+  //       body: raw,
+  //       redirect: "follow",
+  //     };
   
-      // Llama la informacion del Vehiculo por medio de la Placa
-      fetch("https://grupoasistencia.com/motor_webservice/Vehiculo", requestOptions)
-        .then(function (response) {
-          if (!response.ok) {
-            throw Error(response.statusText);
-          }
-          return response.json();
-        })
-        .then(function (myJson) {
-          var estadoConsulta = myJson.Success;
-          var mensajeConsulta = myJson.Message;
+  //     // Llama la informacion del Vehiculo por medio de la Placa
+  //     fetch("https://grupoasistencia.com/motor_webservice/Vehiculo", requestOptions)
+  //       .then(function (response) {
+  //         if (!response.ok) {
+  //           throw Error(response.statusText);
+  //         }
+  //         return response.json();
+  //       })
+  //       .then(function (myJson) {
+  //         var estadoConsulta = myJson.Success;
+  //         var mensajeConsulta = myJson.Message;
   
-          //VALIDA SI LA CONSULTA FUE EXITOSA
-          if (estadoConsulta == true) {
-            var codigoClase = myJson.Data.ClassId;
-            var codigoMarca = myJson.Data.Brand;
-            var modeloVehiculo = myJson.Data.Modelo;
-            var codigoLinea = myJson.Data.BrandLine;
-            var codigoFasecolda = myJson.Data.CodigoFasecolda;
-            var valorAsegurado = myJson.Data.ValorAsegurado;
+  //         //VALIDA SI LA CONSULTA FUE EXITOSA
+  //         if (estadoConsulta == true) {
+  //           var codigoClase = myJson.Data.ClassId;
+  //           var codigoMarca = myJson.Data.Brand;
+  //           var modeloVehiculo = myJson.Data.Modelo;
+  //           var codigoLinea = myJson.Data.BrandLine;
+  //           var codigoFasecolda = myJson.Data.CodigoFasecolda;
+  //           var valorAsegurado = myJson.Data.ValorAsegurado;
   
-            if (codigoFasecolda != null) {
-              if (valorAsegurado == "null" || valorAsegurado == null) {
-                document.getElementById("formularioVehiculo").style.display =
-                  "block";
-                $("#loaderPlaca").html("");
-              } else {
-                var claseVehiculo = "";
-                var limiteRCESTADO = "";
+  //           if (codigoFasecolda != null) {
+  //             if (valorAsegurado == "null" || valorAsegurado == null) {
+  //               document.getElementById("formularioVehiculo").style.display =
+  //                 "block";
+  //               $("#loaderPlaca").html("");
+  //             } else {
+  //               var claseVehiculo = "";
+  //               var limiteRCESTADO = "";
   
-                if (codigoClase == 1) {
-                  claseVehiculo = "AUTOMOVILES";
-                  limiteRCESTADO = 6;
-                } else if (codigoClase == 2) {
-                  claseVehiculo = "CAMPEROS";
-                  limiteRCESTADO = 18;
-                } else if (codigoClase == 3) {
-                  claseVehiculo = "PICK UPS";
-                  limiteRCESTADO = 18;
-                } else if (codigoClase == 4) {
-                  claseVehiculo = "UTILITARIOS DEPORTIVOS";
-                  limiteRCESTADO = 6;
-                } else if (codigoClase == 12) {
-                  claseVehiculo = "MOTOCICLETA";
-                  limiteRCESTADO = 6;
-                } else if (codigoClase == 14) {
-                  claseVehiculo = "PESADO";
-                  limiteRCESTADO = 18;
-                } else if (codigoClase == 19) {
-                  claseVehiculo = "VAN";
-                  limiteRCESTADO = 18;
-                } else if (codigoClase == 16) {
-                  claseVehiculo = "MOTOCICLETA";
-                  limiteRCESTADO = 6;
-                }
+  //               if (codigoClase == 1) {
+  //                 claseVehiculo = "AUTOMOVILES";
+  //                 limiteRCESTADO = 6;
+  //               } else if (codigoClase == 2) {
+  //                 claseVehiculo = "CAMPEROS";
+  //                 limiteRCESTADO = 18;
+  //               } else if (codigoClase == 3) {
+  //                 claseVehiculo = "PICK UPS";
+  //                 limiteRCESTADO = 18;
+  //               } else if (codigoClase == 4) {
+  //                 claseVehiculo = "UTILITARIOS DEPORTIVOS";
+  //                 limiteRCESTADO = 6;
+  //               } else if (codigoClase == 12) {
+  //                 claseVehiculo = "MOTOCICLETA";
+  //                 limiteRCESTADO = 6;
+  //               } else if (codigoClase == 14) {
+  //                 claseVehiculo = "PESADO";
+  //                 limiteRCESTADO = 18;
+  //               } else if (codigoClase == 19) {
+  //                 claseVehiculo = "VAN";
+  //                 limiteRCESTADO = 18;
+  //               } else if (codigoClase == 16) {
+  //                 claseVehiculo = "MOTOCICLETA";
+  //                 limiteRCESTADO = 6;
+  //               }
   
-                $("#CodigoClase").val(codigoClase);
-                $("#txtClaseVeh").val(claseVehiculo);
-                $("#LimiteRC").val(limiteRCESTADO);
-                $("#CodigoMarca").val(codigoMarca);
-                $("#txtModeloVeh").val(modeloVehiculo);
-                $("#CodigoLinea").val(codigoLinea);
-                $("#txtFasecolda").val(codigoFasecolda);
-                $("#txtValorFasecolda").val(valorAsegurado);
+  //               $("#CodigoClase").val(codigoClase);
+  //               $("#txtClaseVeh").val(claseVehiculo);
+  //               $("#LimiteRC").val(limiteRCESTADO);
+  //               $("#CodigoMarca").val(codigoMarca);
+  //               $("#txtModeloVeh").val(modeloVehiculo);
+  //               $("#CodigoLinea").val(codigoLinea);
+  //               $("#txtFasecolda").val(codigoFasecolda);
+  //               $("#txtValorFasecolda").val(valorAsegurado);
   
-                consulDatosFasecolda(codigoFasecolda, modeloVehiculo).then(
-                  function (resp) {
-                    $("#txtMarcaVeh").val(resp.marcaVeh);
-                    $("#txtReferenciaVeh").val(resp.lineaVeh);
-                  }
-                );
-              }
-            }
-          } else {
-            if (
-              mensajeConsulta == "Parámetros Inválidos. Placa es requerido." ||
-              mensajeConsulta == "Favor diligenciar correctamente la placa"
-            ) {
-              swal.fire({ text: "! Favor diligenciar correctamente la placa. ¡" });
-            } else if (
-              mensajeConsulta == "Vehículo no encontrado." ||
-              mensajeConsulta == "Unable to connect to the remote server"
-            ) {
-              document.getElementById("formularioVehiculo").style.display =
-                "block";
-            } else {
-              contErrMetEstado++;
-              if (contErrMetEstado > 1) {
-                document.getElementById("formularioVehiculo").style.display =
-                  "block";
-                contErrMetEstado = 0;
-              } else {
-                setTimeout(consulPlaca, 2000);
-              }
-            }
-            $("#loaderPlaca").html("");
-          }
-        })
-        .catch(function (error) {
-          console.log("Parece que hubo un problema: \n", error);
+  //               consulDatosFasecolda(codigoFasecolda, modeloVehiculo).then(
+  //                 function (resp) {
+  //                   $("#txtMarcaVeh").val(resp.marcaVeh);
+  //                   $("#txtReferenciaVeh").val(resp.lineaVeh);
+  //                 }
+  //               );
+  //             }
+  //           }
+  //         } else {
+  //           if (
+  //             mensajeConsulta == "Parámetros Inválidos. Placa es requerido." ||
+  //             mensajeConsulta == "Favor diligenciar correctamente la placa"
+  //           ) {
+  //             swal.fire({ text: "! Favor diligenciar correctamente la placa. ¡" });
+  //           } else if (
+  //             mensajeConsulta == "Vehículo no encontrado." ||
+  //             mensajeConsulta == "Unable to connect to the remote server"
+  //           ) {
+  //             document.getElementById("formularioVehiculo").style.display =
+  //               "block";
+  //           } else {
+  //             contErrMetEstado++;
+  //             if (contErrMetEstado > 1) {
+  //               document.getElementById("formularioVehiculo").style.display =
+  //                 "block";
+  //               contErrMetEstado = 0;
+  //             } else {
+  //               setTimeout(consulPlaca, 2000);
+  //             }
+  //           }
+  //           $("#loaderPlaca").html("");
+  //         }
+  //       })
+  //       .catch(function (error) {
+  //         console.log("Parece que hubo un problema: \n", error);
   
-          contErrProtocolo++;
-          if (contErrProtocolo > 1) {
-            $("#loaderPlaca").html("");
-            document.getElementById("formularioVehiculo").style.display = "block";
-            contErrProtocolo = 0;
-          } else {
-            setTimeout(consulPlaca, 4000);
-          }
-        });
-    }
-  }
+  //         contErrProtocolo++;
+  //         if (contErrProtocolo > 1) {
+  //           $("#loaderPlaca").html("");
+  //           document.getElementById("formularioVehiculo").style.display = "block";
+  //           contErrProtocolo = 0;
+  //         } else {
+  //           setTimeout(consulPlaca, 4000);
+  //         }
+  //       });
+  //   }
+  // }
   
   
   // Permite consultar la informacion del vehiculo por medio de la Placa (Seguros del Estado)
