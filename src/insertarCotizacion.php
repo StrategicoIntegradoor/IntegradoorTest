acb726d338d56485743df8b4b12e4efb8febe062
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-10-31 17:08:14 -0500

    PDF pesados cambios

--- src/insertarCotizacion.php
+++ src/insertarCotizacion.php
@@ -34,8 +34,8 @@ $idCotizacion = $_POST["idCotizacion"];
 $idUsuario = $_SESSION["idUsuario"];
 $mundial = $_SESSION["mundial"];
 // var_dump($CodigoClase);
-// var_dump($idCotizacion);
-// die();
+var_dump($mundial);
+die();
 
 // VALIDAMOS SI VIENE EL CODIGO DEL CLIENTE Y DE LO CONTRARIO SE CREA EN LA BD
 if ($idCliente == "") {
