422028ad7adf1553461396c51693ccf7b62341e9
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-01 12:25:27 -0500

    Mundial pesados admin cotizaciones

--- modelos/cotizaciones.modelo.php
+++ modelos/cotizaciones.modelo.php
@@ -136,16 +136,28 @@ class ModeloCotizaciones{
 
 		if($fechaInicialCotizaciones == null){
 
+			
 			$anoActual = date("Y"); // Obtener el año actual
-
+			$mesActual = date("m"); // Obtener el mes actual
+		
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla, $tabla2, $tabla3, $tabla4, $tabla5 WHERE $tabla.id_cliente = $tabla2.id_cliente 
 			AND $tabla.id_usuario = $tabla5.id_usuario 
 			AND $tabla2.id_tipo_documento = $tabla3.id_tipo_documento 
 			AND $tabla2.id_estado_civil = $tabla4.id_estado_civil 
 			AND YEAR($tabla.cot_fch_cotizacion) = :anoActual 
+			AND MONTH($tabla.cot_fch_cotizacion) >= :mesInicio 
+			AND MONTH($tabla.cot_fch_cotizacion) <= :mesFin
 			AND usuarios.id_Intermediario = :idIntermediario $condicion");
 		
-			$stmt->bindParam(":anoActual", $anoActual, PDO::PARAM_INT); // Asignar el año actual
+			// Calcular el mes de inicio hace tres meses
+			$mesInicio = ($mesActual - 2) <= 0 ? 12 + ($mesActual - 2) : $mesActual - 2;
+			
+			// Calcular el mes de fin (mes actual)
+			$mesFin = $mesActual;
+		
+			$stmt->bindParam(":anoActual", $anoActual, PDO::PARAM_INT);
+			$stmt->bindParam(":mesInicio", $mesInicio, PDO::PARAM_INT);
+			$stmt->bindParam(":mesFin", $mesFin, PDO::PARAM_INT);
 			$stmt->bindParam(":idIntermediario", $_SESSION["intermediario"], PDO::PARAM_INT);
 		
 			if ($_SESSION["permisos"]["Verlistadodecotizacionesdelaagencia"] != "x") {
