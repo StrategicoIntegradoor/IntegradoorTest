9cbc4b388ccede6c0dc7f8758ffc6fcfcc370667
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-02 07:51:03 -0500

    PDF pesados cambios

--- vistas/modulos/editar-cotizacion.php
+++ vistas/modulos/editar-cotizacion.php
@@ -12,7 +12,9 @@
 // }
 
 ?>
-
+<head>
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+</head>
 <style>
   .botones-agregar-manual {
     margin-top: 20px;
@@ -397,38 +399,39 @@
                   <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtPlacaVeh">Placa</label>
-                      <input type="text" class="form-control" id="txtPlacaVeh" placeholder="" disabled>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-3 form-group">
-                      <label for="txtClaseVeh">Clase</label>
-                      <input type="text" class="form-control" id="txtClaseVeh" placeholder="" disabled>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-3 form-group">
-                      <label for="txtMarcaVeh">Marca</label>
-                      <input type="text" class="form-control classMarcaVeh" id="txtMarcaVeh" placeholder="" disabled>
+                      <input type="text" class="form-control" id="txtPlacaVehPesado" placeholder="" disabled>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtModeloVeh">Modelo</label>
-                      <input type="text" class="form-control" id="txtModeloVeh" placeholder="" disabled>
+                      <input type="text" class="form-control" id="txtModeloVehPesado" placeholder="" disabled>
                     </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 form-group">
+                      <label for="clasepesados">Clase Vehiculo</label>
+                      <input type="text" class="form-control" id="clasepesados" placeholder="" disabled>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 form-group">
+                      <label for="txtMarcaVeh">Marca</label>
+                      <input type="text" class="form-control classMarcaVeh" id="txtMarcaVehPesado" placeholder="" disabled>
+                    </div>
+                    
                   </div>
 
                   <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtReferenciaVeh">Línea</label>
-                      <input type="text" class="form-control classReferenciaVeh" id="txtReferenciaVeh" placeholder="" disabled>
+                      <input type="text" class="form-control classReferenciaVeh" id="txtReferenciaVehPesado" placeholder="" disabled>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtFasecolda">Fasecolda</label>
-                      <input type="text" class="form-control" id="txtFasecolda" placeholder="" required>
+                      <input type="text" class="form-control" id="txtFasecoldaPesado" placeholder="" required>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtValorFasecolda">Valor Asegurado</label>
-                      <input type="text" class="form-control" id="txtValorFasecolda" placeholder="" required>
+                      <input type="text" class="form-control" id="txtValorFasecoldaPesado" placeholder="" required>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtTipoUsoVehiculo">Tipo de Uso</label>
-                      <select class="form-control" id="txtTipoUsoVehiculo" required>
+                      <select class="form-control" id="txtTipoUsoVehiculoPesado" required>
                         <option value=""></option>
                         <option value="Particular" selected>Particular</option>
                         <option value="Trabajo">Trabajo</option>
@@ -439,7 +442,7 @@
                   <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="txtTipoServicio">Tipo de Servicio</label>
-                      <select class="form-control" id="txtTipoServicio" required>
+                      <select class="form-control" id="txtTipoServicioPesado" required>
                         <option value=""></option>
                         <option value="14" selected>Particular</option>
                         <option value="11">Publico Municipal</option>
@@ -449,7 +452,7 @@
 
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="DptoCirculacion">Departamento de Circulación</label>
-                      <select class="form-control" id="DptoCirculacion" required>
+                      <select class="form-control" id="DptoCirculacionPesado" required>
                         <option value=""></option>
                         <option value="1">Amazonas</option>
                         <option value="2">Antioquia</option>
@@ -496,26 +499,20 @@
 
                     <div class="col-xs-12 col-sm-6 col-md-3 form-group">
                       <label for="ciudadCirculacion">Ciudad de Circulación</label>
-                      <select class="form-control" id="ciudadCirculacion" required></select>
+                      <select class="form-control" id="ciudadCirculacionPesado" required></select>
                       <div id="listaCiudades"></div>
                     </div>
 
-                    <div class="col-xs-12 col-sm-6 col-md-3">
-                      <div class="row">
-                        <div class="col-xs-5 col-sm-5 col-md-5 form-group">
-                          <label>Es Oneroso?</label>
-                          <div class="conten-oneroso">
-                            <label for="Si">Si</label>
-                            <input type="radio" name="oneroso" id="esOnerosoSi" value="Si">&nbsp;&nbsp;&nbsp;&nbsp;
-                            <label for="No">No</label>
-                            <input type="radio" name="oneroso" id="esOnerosoNo" value="No" required>
-                          </div>
-                        </div>
-                        <div class="col-xs-7 col-sm-7 col-md-7 form-group" id="contenBenefOneroso">
-                          <label for="benefOneroso">Beneficiario</label>
-                          <input type="text" class="form-control" id="benefOneroso">
-                        </div>
-                      </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 form-group">
+                      <label for="mundialseguros">Tipo Vehiculo Mundial</label>
+                      <select class="form-control" id="mundialseguros" required>
+                        <option value=""></option>
+                        <option value="1">Tractocamión</option>
+                        <option value="2">Camión</option>
+                        <option value="3">Semipesados</option>
+                        <option value="4">Volquetas</option>
+                        <option value="5">Trailers</option>
+                      </select>
                     </div>
                   </div>
 
@@ -905,3 +902,17 @@
 </div>
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+// document.addEventListener('DOMContentLoaded', function() {
+//     var valorMundial = document.getElementById('mundial').value;
+//     console.log(valorMundial);
+
+//     if (valorMundial === null || valorMundial === "") {
+//         document.getElementById('DatosVehiculoPesados').style.display = 'block';
+//         document.getElementById('DatosVehiculo').style.display = 'none';
+//     } else {
+//         document.getElementById('DatosVehiculoPesados').style.display = 'none';
+//         document.getElementById('DatosVehiculo').style.display = 'block';
+//     }
+// });
+</script>
