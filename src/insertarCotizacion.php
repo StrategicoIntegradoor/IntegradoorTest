5b89f2167a14e596de4e1fef50e339a9c669cb96
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-03 09:15:27 -0500

    Pruebas AXA colpatria

--- src/insertarCotizacion.php
+++ src/insertarCotizacion.php
@@ -109,6 +109,9 @@ if($idCotizacion == ""){
 		$cot_codigo = "COT-1";
 	}
 
+	if (!isset($mundial)) {
+		$mundial = NULL;
+	}
 
 	// INSERCIÓN DATOS DE LA COTIZACION REALIZADA
 	$sql = "INSERT INTO `cotizaciones` (`id_cotizacion`, `cot_codigo`, `cot_fch_cotizacion`, `cot_placa`, `cot_cerokm`, `cot_cod_clase`, `cot_clase`, 
@@ -119,8 +122,7 @@ if($idCotizacion == ""){
 
 	$res = mysqli_query($con, $sql);
 	$num_rows = mysqli_affected_rows($con);
-	var_dump($res);
-	die();
+
 
 	if ($num_rows > 0) {
 		$data['Message 2'] = 'Cotización creada exitosamente';
