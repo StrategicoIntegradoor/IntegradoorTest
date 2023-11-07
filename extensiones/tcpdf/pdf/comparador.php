56a3e90dafc328ad3de12e1f6ab9dfcf00f68d1e
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-07 10:23:58 -0500

    Cambios pdf liberty

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -1970,7 +1970,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
 	
     if ($cont26 % 2 == 0) {
         if ($PT == "Deducible: 10% min 1.2 SMMLV" || $PT == "Deducible: 10% min 1 SMMLV") {
-			$html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><div style="font-size:14pt">&nbsp;</div><font size="7"style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
+			$html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;"><center><div style="font-size:7pt">&nbsp;</div><font size="7"style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
         } else {
             if ($rowRespuestaAsistencia22['GastosMedicos'] == "Si ampara") {
                 $html4 .= '<td class="puntos fondo" style="width:' . $valorTabla . '%;text-align: center;"><div style="font-size:12pt">&nbsp;</div><img style="width:16px;" src="../../../vistas/img/logos/cheque.png" alt=""></td>';
@@ -1980,7 +1980,7 @@ while ($rowRespuesta28 = mysqli_fetch_assoc($respuestaquery28)) {
         }
     } else {
         if ($PT == "Deducible: 10% min 1 SMMLV" || $PT == "Deducible: 10% min 1.2 SMMLV") {			
-			$html4 .= '<td class="puntos fondo2" style="width:' . $valorTabla . '%;"><center><div style="font-size:14pt">&nbsp;</div><font size="7"style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
+			$html4 .= '<td class="puntos fondo2" style="width:' . $valorTabla . '%;"><center><div style="font-size:7pt">&nbsp;</div><font size="7"style="text-align: center; font-family:dejavusanscondensed;">' . $respuesta1 . '</font></center></td>';
         } else {
             if ($rowRespuestaAsistencia22['GastosMedicos'] == "Si ampara") {
                 $html4 .= '<td class="puntos fondo2" style="width:' . $valorTabla . '%;text-align: center;"><div style="font-size:12pt">&nbsp;</div><img style="width:16px;" src="../../../vistas/img/logos/cheque.png" alt=""></td>';
