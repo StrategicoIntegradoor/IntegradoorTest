641076e32f7fc55f30aa40b804f6154d9ad1a2d7
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-07 11:32:53 -0500

    Cambios pdf liberty

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -1228,6 +1228,7 @@ $html4 = '
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //CONSULTA GRUA
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+$html4 .= '<table>';
 
 $html4 .= '<tr style="width: 100%;" class="izquierda">';
 $html4 .= '<td style ="width: 100%; background-color: #D1D1D1; font-family:dejavusanscondensedb;" colspan="' . ($fila6 + 1) . '"><div style="font-size:3pt">&nbsp;</div>ASISTENCIAS <div style="font-size:3pt">&nbsp;</div></td>';
