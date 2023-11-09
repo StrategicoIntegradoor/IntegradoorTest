6c1eb8f47fa6fbbb79948d09f3a65eae8598dbfd
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-08 19:05:09 -0500

    MODULO ASSITCARD

--- vistas/modulos/assistcard.php
+++ vistas/modulos/assistcard.php
@@ -256,9 +256,9 @@ select:invalid + .error-message {
 .card-exequias .card-body {
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
     text-align: center;
-    justify-content: center;
     height: 100%;
 }
 
