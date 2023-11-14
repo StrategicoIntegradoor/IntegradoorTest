054135bc3a51cf4c2d1ac3820546126e1e1da92a
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-14 16:31:51 -0500

    CORRECCIONES PRODUCCION MUNDIAL

--- vistas/modulos/pesados.php
+++ vistas/modulos/pesados.php
@@ -537,7 +537,7 @@ if ($_SESSION["permisos"]["Cotizarpesados"] != "x") {
                     <!-- <div id="contenBtnCotizar"> -->
                       <!-- <div class="col-lg-12 conten-cotizar"> -->
                         <!-- <div class="row"> -->
-                          <div class="col-xs-12 col-sm-6 col-md-3 form-group">
+                          <div class="col-xs-12 col-sm-6 col-md-3 form-group" id="contenBtnCotizar">
                             <button class="btn btn-primary btn-block" id="btnCotizarPesados">Cotizar Ofertas</button>
                           </div>
                           <div class="col-xs-12 col-sm-6 col-md-3 form-group">
