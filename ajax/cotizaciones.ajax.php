faaec6eb91223114627a825605e5d648c6e55ba9
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-14 09:14:39 -0500

    CORRECCIONES PRODUCCION MUNDIAL

--- ajax/cotizaciones.ajax.php
+++ ajax/cotizaciones.ajax.php
@@ -32,9 +32,7 @@ class AjaxCotizaciones {
 
 		$item = "id_cotizacion";
 		$valor = $this->idCotizaOferta;
-		var_dump($item);
-        var_dump($valor);
-        die();
+
 		$respuesta = ControladorCotizaciones::ctrMostrarCotizaOfertas($item, $valor);
 
 		echo json_encode($respuesta);
