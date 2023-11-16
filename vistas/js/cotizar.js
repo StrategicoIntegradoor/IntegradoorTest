03780351e3f015a589a199d0f0330ddf806453da
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-16 09:18:32 -0500

    ARREGLO SERVICIO WEB ALLIANZ

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -2064,7 +2064,7 @@ function cotizarOfertas() {
 
             /* Allianz */
             cont.push(
-              fetch("https://grupoasistencia.com/motor_webservice_tst/Allianz", requestOptions)
+              fetch("https://grupoasistencia.com/motor_webservice/Allianz", requestOptions)
                 .then((res) => {
                   if (!res.ok) throw Error(res.statusText);
                   return res.json();
