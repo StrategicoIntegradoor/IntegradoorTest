4882391065a5d25170ecb5387b03e744547fef8c
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-16 14:15:45 -0500

    MODULO VIAJES

--- vistas/modulos/assistcard.php
+++ vistas/modulos/assistcard.php
@@ -159,7 +159,7 @@
   border-radius: 20px;
   background-color: white;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-  padding: 9%;
+  padding: 8%;
   max-width: 90%;  
   margin: 0 auto;  
   /* min-height: 370px; */
