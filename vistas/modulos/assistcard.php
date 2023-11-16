5350c76e54543cf182cfaf3f2b5d3660f467e7cf
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-16 12:50:22 -0500

    MODULO VIAJES

--- vistas/modulos/assistcard.php
+++ vistas/modulos/assistcard.php
@@ -393,9 +393,11 @@ select:invalid + .error-message {
             <!-- //GENERADOR DE PDF -->
             <br>
             <div class="row">
-                <div class="col-md-4 col-sm-12 mb-3">
-                    <!-- TITULO GENERADOR DE PDF -->
-                    <h4 style="font-family: 'Arial Arabic', Arial; font-weight: bold; margin-bottom: 3px; margin-top: 3px; margin-left: 7.5%;">Solicita una cotización en el siguiente formulario</h4>
+                <div class="content">
+                    <div class="col-md-4 col-sm-12 mb-3">
+                        <!-- TITULO GENERADOR DE PDF -->
+                        <h4 style="font-family: 'Arial Arabic', Arial; font-weight: bold; margin-bottom: 3px; margin-top: 3px; margin-left: 7.5%;">Solicita una cotización en el siguiente formulario</h4>
+                    </div>
                 </div>
             </div>
 
