033cb235817962359e9ec4a469747c80f7deb811
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-11-20 16:45:34 -0500

    ULTIMOS ARREGLOS ASSISTCARD

--- vistas/modulos/assistcard.php
+++ vistas/modulos/assistcard.php
@@ -411,7 +411,7 @@ select:invalid + .error-message {
                         <!-- TITULO FORMULARIO VIAJES -->
                     <h4 style="font-family: 'Arial Arabic', Arial; font-weight: bold; margin-bottom: 5px; margin-top: 3px;">Solicita una cotización en el siguiente formulario:</h4>
                     <div style="width: 100%; max-width: 640px; margin: 0 auto; margin-top: 30px; margin-bottom: -2px">
-                        <img src="vistas/img/plantilla/banner_viajes.png" alt="Banner de Viajes" style="width: 640px; height: auto; display: block; margin: 0 auto;">
+                        <img src="vistas/img/plantilla/banner_viajes.png" alt="Banner de Viajes" style="width: 590px; height: auto; display: block; margin: 0 auto;">
                         <iframe class="miIframe" src="https://docs.google.com/forms/d/e/1FAIpQLSdNXEYeuq8L5G15BQpGNKKt12cs7jzGvxYuqw2gdQaIH3qwGw/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
                     </div>
                 </div>
