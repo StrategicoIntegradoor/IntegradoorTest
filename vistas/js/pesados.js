a38e631fe9e9fb4c05477802297ab0d1ee39dac0
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-04 17:03:00 -0500

    COTIZAR BOLIVAR 0km

--- vistas/js/pesados.js
+++ vistas/js/pesados.js
@@ -105,7 +105,7 @@ $(document).ready(function () {
     $("#txtConocesLaPlacaNo").click(function () {
       document.getElementById("contenPlaca").style.display = "none";
       document.getElementById("contenCeroKM").style.display = "block";
-      document.getElementById("placaVeh").value = "WWW408";
+      document.getElementById("placaVeh").value = "WWW404";
       $("#txtEsCeroKmNo").prop("checked", false);
     });
   
@@ -958,7 +958,7 @@ function masAseg() {
           $("#clasepesados").val(clase);
   
           var placaVeh = $("#placaVeh").val();
-          if (placaVeh == "WWW408") {
+          if (placaVeh == "WWW404") {
             $("#txtPlacaVeh").val("SIN PLACA - VEHÍCULO 0 KM").val();
           } else {
             $("#txtPlacaVeh").val(placaVeh).val();
@@ -1793,7 +1793,7 @@ function cotizarOfertasPesados() {
            $("#clasepesados").val(clase);
 
            var placaVeh = $("#placaVeh").val();
-           if (placaVeh == "WWW408") {
+           if (placaVeh == "WWW404") {
              $("#txtPlacaVeh").val("SIN PLACA - VEHÍCULO 0 KM").val();
            } else {
              $("#txtPlacaVeh").val(placaVeh).val();
