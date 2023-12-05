3d6fd937ef653c23919b3b9c937de5e3a1ccbfe2
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-05 17:10:45 -0500

    PRUEBAS 0KM

--- extensiones/tcpdf/pdf/comparador.php
+++ extensiones/tcpdf/pdf/comparador.php
@@ -58,7 +58,7 @@ $fechaCotiz = substr($fila['cot_fch_cotizacion'], 0, -9);
 $fechaVigencia = date("d-m-Y", strtotime($fechaCotiz));
 
 $placa = $fila["cot_placa"] . " ";
-if ($placa == "CAT770 ") {
+if ($placa == "WWW404") {
 	$placa = "  0 KMM";
 }
 $modelo = $fila["cot_modelo"];
