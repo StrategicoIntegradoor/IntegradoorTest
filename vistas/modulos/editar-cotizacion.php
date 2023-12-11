c9d0c86984cdefb526cfb63a7ffc14a09ccf9da5
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-11 09:43:47 -0500

    TABLA RESUMEN COTIZACIONES

--- vistas/modulos/editar-cotizacion.php
+++ vistas/modulos/editar-cotizacion.php
@@ -844,7 +844,7 @@
 
         <!-- PARRILLA DE COTIZACIONES -->
         <div id="contenParrilla">
-          <div class="col-lg-12">
+          <div class="col-lg-12 form-parrilla">
             <div class="row row-parrilla">
               <div class="col-xs-12 col-sm-6 col-md-3">
                 <label for="">RESUMEN DE COTIZACIONES</label>
@@ -853,12 +853,12 @@
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-3">
                 </div>
-                <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="col-xs-12 col-sm-6 col-md-3 text-right">
                   <div id="masResOferta">
-                    <p id="masResumen" onclick="masRE();">Ver mas<i class="fa fa-plus-square-o"></i></p>
+                    <p id="masResumen" onclick="masRE();">Ver mas <i class="fa fa-plus-square-o"></i></p>
                   </div>
                   <div id="menosResOferta">
-                    <p id="menosResumen" onclick="menosRE();">Ver menos<i class="fa fa-minus-square-o"></i></p>
+                    <p id="menosResumen" onclick="menosRE();">Ver menos <i class="fa fa-minus-square-o"></i></p>
                   </div>
                 </div>
             </div>
