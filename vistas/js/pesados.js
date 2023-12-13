b7145c6546204a0b98dbcd58fe7b26ab10a9ff78
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-13 12:30:56 -0500

    CAMBIOS MODULO DE PESADOS

--- vistas/js/pesados.js
+++ vistas/js/pesados.js
@@ -1125,22 +1125,21 @@ function registrarOfertaPesados(
               <div class='col-lg-12'>
                 <div class='card-ofertas'>
                   <div class='row card-body'>
-                    <div class="col-xs-12 col-sm-6 col-md-2 oferta-logo">
+                  <div class="col-xs-12 col-sm-6 col-md-2 oferta-logo" style="position: relative; height: 100%;">
+                    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
                       <center>
-    
-                      <img src='vistas/img/logos/${logo}'>
-
+                          <img src='vistas/img/logos/${logo}'>
                       </center>  
-    
                       <div class='col-12' style='margin-top:2%;'>
-                      ${aseguradora !== "Mundial" && permisos.Vernumerodecotizacionencadaaseguradora == "x" ?
-                        `<center>
-                          <label class='entidad'>N° Cot: <span style ='color :black'>${numCotizOferta}</span></label>
-                        </center>`
-                        : ''}
-                            </div>
-  
+                          ${aseguradora !== "Mundial" && permisos.Vernumerodecotizacionencadaaseguradora == "x" ?
+                            `<center>
+                                <label class='entidad'>N° Cot: <span style ='color :black'>${numCotizOferta}</span></label>
+                            </center>`
+                            : ''}
+                      </div>
                     </div>
+                  </div>
+               
                     <div class="col-xs-12 col-sm-6 col-md-2 oferta-header">
                       <h5 class='entidad'>${aseguradora} - ${producto}</h5>
                       <h5 class='precio'>Desde $ ${prima}</h5>
