bdef1537f5af01a668f750b83f975c44de698b8e
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-13 15:08:58 -0500

    CAMBIOS MODULO DE PESADOS

--- vistas/js/pesados.js
+++ vistas/js/pesados.js
@@ -1127,9 +1127,7 @@ function registrarOfertaPesados(
                   <div class='row card-body'>
 
                   <div class="col-xs-12 col-sm-6 col-md-2 oferta-logo" style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-                      <center>
-                        <img src='vistas/img/logos/${logo}'>
-                      </center>  
+                      <img src='vistas/img/logos/${logo}'>
                     <div class='col-12' style='margin-top:-2%;'>
                       ${aseguradora !== "Mundial" && permisos.Vernumerodecotizacionencadaaseguradora == "x" ?
                         `<center>
