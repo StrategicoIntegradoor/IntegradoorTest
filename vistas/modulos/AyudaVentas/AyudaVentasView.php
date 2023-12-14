7bec54bd4731f24054123e8081b9af5056dd90b5
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-14 15:41:13 -0500

    CAMBIOS AYUDAVENTAS

--- vistas/modulos/AyudaVentas/AyudaVentasView.php
+++ vistas/modulos/AyudaVentas/AyudaVentasView.php
@@ -159,7 +159,7 @@ if ($_SESSION["permisos"]["AyudaVentasFreelance"] == "x") {
 
 
                 <div class="table-responsive table_media">
-                    <table class="table-media">
+                    <table class="table table-bordered tablas-asistencias table-media">
 
                         <thead style="background: #88d600; color: #FFF; ">
                             <tr>
