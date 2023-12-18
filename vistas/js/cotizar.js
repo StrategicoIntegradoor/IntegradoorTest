a87d16f472ae1472a4edd8c5cd8dcafbf8fcdbc1
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-18 14:49:53 -0500

    PRUEBAS PESADOS Y MOTOS AXA

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -1860,41 +1860,41 @@ function cotizarOfertas() {
             // );
 
             /* Mapfre */
-            cont.push(
+            // cont.push(
 
-              fetch("https://grupoasistencia.com/motor_webservice_tst/mapfrecotizacion4", requestOptions)
+            //   fetch("https://grupoasistencia.com/motor_webservice_tst/mapfrecotizacion4", requestOptions)
 
-                .then((res) => {
-                  if (!res.ok) throw Error(res.statusText);
-                  return res.json();
-                })
-                .then((ofertas) => {
-                  console.log(ofertas)
-                  let result = []
-                  result.push(ofertas)
+            //     .then((res) => {
+            //       if (!res.ok) throw Error(res.statusText);
+            //       return res.json();
+            //     })
+            //     .then((ofertas) => {
+            //       console.log(ofertas)
+            //       let result = []
+            //       result.push(ofertas)
 
-                  if (typeof ofertas[0].Resultado !== 'undefined') {
-                    agregarAseguradoraFallida('Mapfre')
-                    ofertas[0].Mensajes.forEach(mensaje => {
-                      mostrarAlertarCotizacionFallida('Mapfre', mensaje)
-                    })
+            //       if (typeof ofertas[0].Resultado !== 'undefined') {
+            //         agregarAseguradoraFallida('Mapfre')
+            //         ofertas[0].Mensajes.forEach(mensaje => {
+            //           mostrarAlertarCotizacionFallida('Mapfre', mensaje)
+            //         })
 
-                  } else {
+            //       } else {
 
-                    validarOfertas(ofertas);
-                    // let successMap = true;
-                    // if (successMap) {
-                      mostrarAlertaCotizacionExitosa('Mapfre')
-                      // successMap = false
-                    // }
-                  }
-                })
-                .catch((err) => {
-                  agregarAseguradoraFallida('Mapfre');
-                  mostrarAlertarCotizacionFallida('Mapfre', "Error de servicio (500), intente de nuevo");
-                  console.error(err);
-                })
-            );
+            //         validarOfertas(ofertas);
+            //         // let successMap = true;
+            //         // if (successMap) {
+            //           mostrarAlertaCotizacionExitosa('Mapfre')
+            //           // successMap = false
+            //         // }
+            //       }
+            //     })
+            //     .catch((err) => {
+            //       agregarAseguradoraFallida('Mapfre');
+            //       mostrarAlertarCotizacionFallida('Mapfre', "Error de servicio (500), intente de nuevo");
+            //       console.error(err);
+            //     })
+            // );
 
             /* Previsora */
             // cont.push(
