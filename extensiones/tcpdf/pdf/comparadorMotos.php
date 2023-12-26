3f3ca1f2c624bde1fd59895d3b7a30709e3a79cc
oscaruv <guerrero.oscar@correounivalle.edu.co>
2023-12-25 22:10:25 -0500

    Pruebas pdf motos

--- extensiones/tcpdf/pdf/comparadorMotos.php
+++ extensiones/tcpdf/pdf/comparadorMotos.php
@@ -1039,9 +1039,9 @@ $cont5 = 1;
 while ($rowRespuesta8 = mysqli_fetch_assoc($respuestaquery8)) {
 
 	$nombreAseguradora = nombreAseguradora($rowRespuesta8['Aseguradora']);
-    var_dump($nombreAseguradora);
-    die();
 	$nombreProducto = productoAseguradora($rowRespuesta8['Aseguradora'], $rowRespuesta8['Producto']);
+    var_dump($nombreProducto);
+    die();
 	$valorRC = $rowRespuesta8['ValorRC'];
 	$perdidaParcial = $rowRespuesta8['PerdidaParcial'];
 
