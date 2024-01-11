8f356454406933f8631b0623e3f7e5fb6e856cea
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-01-11 16:58:25 -0500

    Header version movil

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -4,6 +4,23 @@
      
      <style>
 
+		@media (max-width: 450px) {
+			.main-header {
+				/* Ajusta la altura según tus necesidades */
+				height: 80px !important;
+			}
+
+			/* Otros estilos específicos para la versión móvil si es necesario */
+			.logo-mini img,
+			.logo-lg img {
+				/* Ajusta el tamaño del logo si es necesario */
+				max-height: 40px;
+			}
+
+			/* Ajusta otros estilos según tus necesidades */
+			/* Agrega estilos adicionales según sea necesario para otros elementos dentro del encabezado */
+		}
+
         @media (max-width: 450px){
             .li_cotDi{
                 font-size: 15px !important;
