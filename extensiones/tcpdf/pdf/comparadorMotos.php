d2cdd259ac5c8ccfa390d95bd7c7ba875c3bc0ff
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-01-12 15:42:34 -0500

    Correccion imagen pdf motos

--- extensiones/tcpdf/pdf/comparadorMotos.php
+++ extensiones/tcpdf/pdf/comparadorMotos.php
@@ -170,7 +170,7 @@ $pdf->Cell(25, 6, "No. cotización: " . $identificador);
 
 $pdf->Image(
     '../../../vistas/img/logos/moto.jpg', // Ruta de la imagen
-    18, // Posición X (izquierda)
+    26, // Posición X (izquierda)
     126.5, // Posición Y
     40, // Ancho (ajustar según sea necesario)
     0, // Altura (0 indica autoajuste proporcional)
