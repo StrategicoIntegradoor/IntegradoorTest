6101db80b6c4102ae7e82d6f52d6254a67584d4b
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-01-15 10:46:20 -0500

    PDF LIBERTY MOTOS CORRECIONES

--- vistas/js/motos.js
+++ vistas/js/motos.js
@@ -747,12 +747,10 @@ function registrarOfertaMotos(
                           <span class="badge">* ${CE}</span>
                           Conductor elegido
                         </li>
-                        ${aseguradora !== "Liberty" ?
-                        `<li class="list-group-item">
+                        <li class="list-group-item">
                           <span class="badge">* ${GR}</span>
                           Servicio de Grúa
-                        </li>`
-                          : ''}
+                        </li>
                       </ul>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-2">
