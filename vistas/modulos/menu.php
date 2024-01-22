cd79a8aedfcec05720b19a43068359a6f7c08914
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-01-22 14:58:47 -0500

    MODULO SOAT

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -135,7 +135,7 @@
 			echo '<li role="presentation" style="width: 50px; height: 44px;">
 					<a href="exequias">
 						<span>SOAT</span>
-						<img class="imagen" width="14" height="14" src="vistas/img/plantilla/soat.jpg" alt="SOAT">
+						<img class="imagen" width="17" height="17" src="vistas/img/plantilla/soat.jpg" alt="SOAT">
 					</a>
 				  </li>';
 		}
