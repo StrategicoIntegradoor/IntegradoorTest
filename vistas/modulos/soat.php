c7f1680960fcd7fc50a320781bcfc05847438c26
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-01-24 11:52:58 -0500

    MODULO DE SOAT

--- vistas/modulos/soat.php
+++ vistas/modulos/soat.php
@@ -362,11 +362,11 @@
                             <div class="card-exequias">
                                 <div class="card-body">
                                     <h4 class="card-title" style="font-weight: bold;">TIEMPO DE RESPUESTA</h4>
-                                    <p class="card-text">Cotizaciones:</p>
+                                    <h6 class="card-text">Cotizaciones:</h6>
                                     <ul class="card-text" style="padding-left: 0px; list-style-position: inside;">
                                         <li>De <b>30</b> a <b>45</b> minutos hábiles una vez registrado correctamente el formulario</li>
                                     </ul>
-                                    <p class="card-text">Expendiciones:</p>
+                                    <h6 class="card-text">Expendiciones:</h6>
                                     <ul class="card-text" style="padding-left: 0px; list-style-position: inside;">
                                         <li>Vehículos sin problemas en el RUNT: de <b>2</b> hasta <b>5</b> horas hábiles.</li>
                                         <li>Vehículos con problemas en el RUNT: de <b>5</b> horas hasta <b>1</b> día hábil.</li>
