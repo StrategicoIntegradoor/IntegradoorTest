05bb995307196b9d921bbf977ed4caa6098f1bc9
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-01-25 14:29:03 -0500

    ULTIMAS MODIFICACIONES MODULO DE SOAT

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -134,8 +134,8 @@
 		if ($_SESSION["permisos"]["SeguroExequial"] == "x") {
 			echo '<li role="presentation" style="width: 50px; height: 44px;">
 					<a href="soat">
-						<span>SOAT</span>
 						<img class="imagen" style="margin-left: -3px;" width="20" height="20" src="vistas/img/plantilla/soat.jpg" alt="SOAT">
+						<span>SOAT</span>
 					</a>
 				  </li>';
 		}
