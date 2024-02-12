6291ebc809986a96e145ba0687a8ea662218b251
oscaruv <guerrero.oscar@correounivalle.edu.co>
2024-02-12 15:48:21 -0500

    CAMBIOS EN LA PRIMA JUSTA

--- vistas/js/cotizar.js
+++ vistas/js/cotizar.js
@@ -2392,33 +2392,8 @@ $(document).ready(function () {
     
                   /* Liberty */
 
-                //   cont.push(
-                //     fetch("https://grupoasistencia.com/motor_webservice_tst2/Liberty?callback=myCallback", requestOptions)
-                //       .then((res) => {
-                //         if (!res.ok) throw Error(res.statusText);
-                //         return res.json();
-                //       })
-                //       .then((ofertas) => {
-                //         if (typeof ofertas[0].Resultado !== 'undefined') {
-                //           agregarAseguradoraFallida('Liberty');
-                //           validarProblema('Liberty', ofertas);
-                //           ofertas[0].Mensajes.forEach(mensaje => {
-                //             mostrarAlertarCotizacionFallida('Liberty', mensaje)
-                //           })
-                //         } else {
-                //           const contadorPorEntidad = validarOfertas(ofertas,'Liberty', 1);
-                //           mostrarAlertaCotizacionExitosa('Liberty', contadorPorEntidad)
-                //         }
-                //       })
-                //       .catch((err) => {
-                //         agregarAseguradoraFallida('Liberty')
-                //         mostrarAlertarCotizacionFallida('Liberty', "Error de conexión. Intente de nuevo o comuníquese con el equipo comercial");
-                //         console.error(err);
-                //       })
-                //   );
-
                   cont.push(
-                    fetch("https://grupoasistencia.com/motor_webservice_tst3/Liberty?callback=myCallback", requestOptions)
+                    fetch("https://grupoasistencia.com1234/motor_webservice_tst2/Liberty?callback=myCallback", requestOptions)
                       .then((res) => {
                         if (!res.ok) throw Error(res.statusText);
                         return res.json();
@@ -2441,6 +2416,31 @@ $(document).ready(function () {
                         console.error(err);
                       })
                   );
+
+                  // cont.push(
+                  //   fetch("https://grupoasistencia.com/motor_webservice_tst/Liberty?callback=myCallback", requestOptions)
+                  //     .then((res) => {
+                  //       if (!res.ok) throw Error(res.statusText);
+                  //       return res.json();
+                  //     })
+                  //     .then((ofertas) => {
+                  //       if (typeof ofertas[0].Resultado !== 'undefined') {
+                  //         agregarAseguradoraFallida('Liberty');
+                  //         validarProblema('Liberty', ofertas);
+                  //         ofertas[0].Mensajes.forEach(mensaje => {
+                  //           mostrarAlertarCotizacionFallida('Liberty', mensaje)
+                  //         })
+                  //       } else {
+                  //         const contadorPorEntidad = validarOfertas(ofertas,'Liberty', 1);
+                  //         mostrarAlertaCotizacionExitosa('Liberty', contadorPorEntidad)
+                  //       }
+                  //     })
+                  //     .catch((err) => {
+                  //       // agregarAseguradoraFallida('Liberty')
+                  //       mostrarAlertarCotizacionFallida('Liberty', "Servicio en mantenimiento");
+                  //       console.error(err);
+                  //     })
+                  // );
       
                   /* Allianz */
                   cont.push(
